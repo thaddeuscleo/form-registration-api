7fc109d5538c0597c175e3ee8214b15f2046c23c
Thaddeus Cleo <thaddeuscleo@gmail.com>
2023-03-29 13:48:26 +0700

    fix: remove validation for uuid

--- src/bookings/dto/update-booking.input.ts
+++ src/bookings/dto/update-booking.input.ts
@@ -1,10 +1,8 @@
 import { CreateBookingInput } from './create-booking.input';
 import { InputType, Field, PartialType, ID } from '@nestjs/graphql';
-import { IsUUID } from 'class-validator';
 
 @InputType()
 export class UpdateBookingInput extends PartialType(CreateBookingInput) {
-  @IsUUID('4')
   @Field(() => ID)
   id: string;
 }
